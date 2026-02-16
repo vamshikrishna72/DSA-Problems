009819ff75b113749d917309dd0990bf4e32e0d7
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-02-16 22:08:34 +0530

    Time: 0 ms (100%), Space: 8.1 MB (93.3%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,7 +4,7 @@ public:
         int result = 0;
         
         for (int i = 0; i < 32; i++) {
-            result <<= 1;          
+            result<<= 1;          
             result |= (n & 1);     
             n >>= 1;               
         }
