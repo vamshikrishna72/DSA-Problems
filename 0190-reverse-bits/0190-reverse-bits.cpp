a23b99896da7e75530b7c04888e9a91dd3aa8ae7
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-02-16 22:10:29 +0530

    Time: 0 ms (100%), Space: 8.2 MB (73.18%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        int result = 0;
-        
-        for (int i = 0; i < 32; i++) {
-            result<<= 1;          
-            result |= (n & 1);     
+        int res = 0;
+        for (int i = 1; i <= 32; i++) {
+            res<<= 1;          
+            res |= (n & 1);     
             n >>= 1;               
         }
-        
-        return result;
+        return res;
     }
 };
