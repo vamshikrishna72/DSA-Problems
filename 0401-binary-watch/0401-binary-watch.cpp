51994338310562f50b35f1903d2af8150a21f4e4
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-02-17 23:21:10 +0530

    Time: 2 ms (22.13%), Space: 9.6 MB (85.28%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+        
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    
+                    string time = to_string(h) + ":";
+                    
+                    if (m < 10)
+                        time += "0";
+                    
+                    time += to_string(m);
+                    
+                    result.push_back(time);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
