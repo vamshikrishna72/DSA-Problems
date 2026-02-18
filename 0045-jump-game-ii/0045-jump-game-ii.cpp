36fe70f07e04c6e42ead884dbf1b35c0448a6b33
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-02-18 14:54:34 +0530

    Time: 211 ms (7.52%), Space: 22.7 MB (7.15%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,56 @@
+// class Solution {
+// private:
+//     int dfs(vector<int>& nums, int ind, vector<int>& dp) {
+//         int n = nums.size();
+//         if (ind >= n - 1) return 0;
+//         if (dp[ind] != -1) return dp[ind];
+//         int minJumps = INT_MAX;
+        
+//         for (int i = 1; i <= nums[ind]; i++) {
+//             int next = ind + i;
+//             if (next < n) {
+//                 int jumps = dfs(nums, next, dp);
+//                 if (jumps != INT_MAX)
+//                     minJumps = min(minJumps, 1 + jumps);
+//             }
+//         }
+        
+//         return dp[ind] = minJumps;
+//     }
+
+// public:
+//     int jump(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n, -1);
+//         return dfs(nums, 0, dp);
+//     }
+// };
+
+
 class Solution {
 private:
     int dfs(vector<int>& nums, int ind, vector<int>& dp) {
         int n = nums.size();
-        if (ind >= n - 1) return 0;
-        if (dp[ind] != -1) return dp[ind];
-        int minJumps = INT_MAX;
         
-        for (int i = 1; i <= nums[ind]; i++) {
-            int next = ind + i;
+        if (ind >= n - 1) return 0;  
+        
+        if (dp[ind] != -1) return dp[ind];
+        
+        int minSteps = INT_MAX;
+        
+        for (int step = 1; step <= nums[ind]; step++) {
+            int next = ind + step;
+            
             if (next < n) {
                 int jumps = dfs(nums, next, dp);
-                if (jumps != INT_MAX)
-                    minJumps = min(minJumps, 1 + jumps);
+                
+                if (jumps != INT_MAX) {
+                    minSteps = min(minSteps, 1 + jumps);
+                }
             }
         }
         
-        return dp[ind] = minJumps;
+        return dp[ind] = minSteps;
     }
 
 public:
