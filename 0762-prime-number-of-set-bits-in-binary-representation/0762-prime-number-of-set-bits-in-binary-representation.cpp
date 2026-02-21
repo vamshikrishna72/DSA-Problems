fcd84c6a76980330ab63326512ba663d445e976c
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-02-21 12:38:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (35.12%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,53 +1,23 @@
-// class Solution {
-// private: 
-//     int isprime(int n){
-//         if(n<2) return false;
-//         for(int i=2;i*i<=n;i++){
-//             if(n%2 == 0){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// public:
-//     int countPrimeSetBits(int left, int right) {
-//         int count = 0;
-//         for(int i=left;i<=right;i++){
-//             int check = __builtin_popcount(i);
-//             if(isprime(check)){
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-// };
-
-
 class Solution {
-private:
-    bool isPrime(int n) {
-        if (n < 2) return false;
-        
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0)
+private: 
+    int isprime(int n){
+        if(n<2) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i == 0){
                 return false;
+            }
         }
-        
         return true;
     }
-
 public:
     int countPrimeSetBits(int left, int right) {
         int count = 0;
-
-        for (int i = left; i <= right; i++) {
-            int setBits = __builtin_popcount(i); 
-            
-            if (isPrime(setBits)) {
+        for(int i=left;i<=right;i++){
+            int check = __builtin_popcount(i);
+            if(isprime(check)){
                 count++;
             }
         }
-
         return count;
     }
 };
