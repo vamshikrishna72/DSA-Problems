f114e6ace1ee4046c3439497c06edac3f1c1e58e
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-03-05 23:41:59 +0530

    Time: 16 ms (58.2%), Space: 47 MB (86.57%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int>findPSE(const vector<int>& arr, int n) {
         vector<int> left(n, -1);
-        stack<int> st; // Stores indices
+        stack<int> st;
         for (int i = 0; i < n; ++i) {
             while (!st.empty() && arr[st.top()] >= arr[i]) {
                 st.pop();
