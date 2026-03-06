fd8d0947757963813ba7f2015d6103059fd4382e
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-03-06 12:20:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (96.03%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int segment_count = 0;
-        for (int i = 0; i < s.length() - 1; ++i) {
-            if (s[i] == '0' && s[i+1] == '1') {
-                return false; 
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            if(s[i] == '0' && s[i+1] == '1'){
+                return false;
             }
         }
         return true;
