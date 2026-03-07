2bba16167b14dbbdf419fdf095493643f479d1dd
Vamshi Krishna <153121080+vamshikrishna72@users.noreply.github.com>
2026-03-07 23:07:19 +0530

    Time: 16 ms (62%), Space: 17.3 MB (54%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,34 +3,27 @@ public:
     int minFlips(string s) {
         int n = s.size();
         string t = s + s;
-
-        int diff1 = 0, diff2 = 0;
+        int diff1 = 0;
+        int diff2 = 0;
         int res = INT_MAX;
         int l = 0;
-
         for (int r = 0; r < 2 * n; r++) {
-            
-            char expected1 = (r % 2) ? '1' : '0'; // pattern 0101...
-            char expected2 = (r % 2) ? '0' : '1'; // pattern 1010...
-
+            char expected1 = (r % 2) ? '1' : '0';
+            char expected2 = (r % 2) ? '0' : '1';
             if (t[r] != expected1) diff1++;
             if (t[r] != expected2) diff2++;
-
             if (r - l + 1 > n) {
                 char leftExpected1 = (l % 2) ? '1' : '0';
                 char leftExpected2 = (l % 2) ? '0' : '1';
-
                 if (t[l] != leftExpected1) diff1--;
                 if (t[l] != leftExpected2) diff2--;
 
                 l++;
             }
-
             if (r - l + 1 == n) {
                 res = min(res, min(diff1, diff2));
             }
         }
-
         return res;
     }
 };
