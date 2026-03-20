436cb2ed653fc01c32a8bffdbb4524493c658055
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-03-20 22:24:46 +0530

    Time: 62 ms (5.98%), Space: 84.9 MB (63.04%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,10 +1,48 @@
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+//         int maxLen = 0,l=0,r=0;
+//         unordered_map<int,int>mpp;
+//         int n=fruits.size();
+//         while(r < n){
+//             mpp[fruits[r]]++;
+//             if(mpp.size() > 2){
+//                 mpp[fruits[l]]--;
+//                 if(mpp[fruits[l]] == 0) mpp.erase(fruits[l]);
+//                 l++;
+//             }
+//             if(mpp.size() <= 2){
+//                 maxLen = max(maxLen,r-l+1);
+//                 r++;
+//             }
+//         }
+//         return maxLen;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int count = 0;
-        for(int i : fruits){
-            if(i > 0) count++;
+        int maxLen = 0, l = 0, r = 0;
+        unordered_map<int, int> mpp; 
+        int n = fruits.size();
+        
+        while (r < n) {
+            mpp[fruits[r]]++;
+            
+            if (mpp.size() > 2) {
+                mpp[fruits[l]]--;
+                if (mpp[fruits[l]] == 0) mpp.erase(fruits[l]);
+                l++;
+            }
+            
+            if (mpp.size() <= 2) {
+                maxLen = max(maxLen, r - l + 1);
+            }
+            r++;
         }
-        return count;
+        return maxLen;
     }
 };
