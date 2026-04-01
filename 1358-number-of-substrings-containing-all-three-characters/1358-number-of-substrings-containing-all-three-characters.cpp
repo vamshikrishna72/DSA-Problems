3cd752183fed97b5e5d13674c4457b81ef220f80
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-04-01 19:31:15 +0530

    Time: 4 ms (78.43%), Space: 11 MB (56.58%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -6,7 +6,8 @@ public:
         vector<int> lastSeen(3,-1);
         for(int i=0;i<n;i++){
             lastSeen[s[i]-'a']=i;
-            cnt = cnt + (1 + min(lastSeen[0],min(lastSeen[1],lastSeen[2])));
+            if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1)
+                cnt = cnt + (1 + min(lastSeen[0],min(lastSeen[1],lastSeen[2])));
         }
         return cnt;
     }
