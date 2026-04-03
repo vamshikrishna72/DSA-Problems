e07e13339a22085a3796b626aaceabf93959c65e
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-04-03 19:32:24 +0530

    Time: 3 ms (78.12%), Space: 11.5 MB (62.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int m=t.length(),n=s.length();
+        int cnt=0,minLen=INT_MAX,startInd=-1;
         int l=0,r=0;
-        int startInd = -1,cnt=0;
-        int minLen = INT_MAX;
         vector<int>hash(256,0);
-        int n=s.length(),m=t.length();
         for(int i=0;i<m;i++){
             hash[t[i]]++;
         }
         while(r < n){
-            if(hash[s[r]] > 0) cnt++;
+
+            if(hash[s[r]]>0){
+                cnt++;
+            }
             hash[s[r]]--;
             while(cnt == m){
-            if((r-l+1) < minLen){
-                minLen=r-l+1;
-                startInd=l;
+                if(r-l+1 < minLen){
+                    minLen = r-l+1;
+                    startInd = l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]] > 0) cnt--;
+                l++;
             }
-            hash[s[l]]++;
-            if(hash[s[l]]>0) cnt--;
-            l++;
+            r++;
         }
-        r++;
-        }
-        return startInd == -1 ? "":s.substr(startInd,minLen);
-
+        return startInd == -1?"":s.substr(startInd,minLen);
     }
 };
