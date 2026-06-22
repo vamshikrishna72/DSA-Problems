af2cc0fbdd42f2ca2efab1c4a0b4801f25919da7
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-06-22 14:33:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.16%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        int res =0;
+        int res = 0;
+        
         while(x != 0){
-            int last_dig = x % 10;
-            x = x/10;
-            if(res > INT_MAX/10 || (res == INT_MAX/10 && last_dig > 7)) return 0;
-            if(res < INT_MIN/10 || (res == INT_MIN/10 && last_dig < -8)) return 0;
-            res = res*10+last_dig;
+            int temp = x % 10;
+            if (res > INT_MAX / 10 || res < INT_MIN / 10) {
+                return 0;
+            }
+            res = (res * 10) + temp;
+            x = x / 10;
         }
+        
         return res;
     }
 };
