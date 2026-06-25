8483bd1afb57b278c65909a584987cd2334d6347
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-06-25 11:01:42 +0530

    Time: 0 ms (100%), Space: 11.2 MB (82.1%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,15 +1,43 @@
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int n = nums.size();
+//         int low = 0;
+//         int high = n - 1;
+//         while(low < high){
+//             int mid = (low + (high - low))/2;
+//             if(nums[mid] > nums[high]){
+//                 low = mid+1;
+//             }
+//             else{
+//                 high=mid;
+//             }
+//         }
+//         for(int i=low;i<n-1;i++){
+//             if(nums[i] > nums[i+1]) return false;
+//             continue;
+//         }
+//         for(int i=0;i<low;i++){
+//             if(nums[i] > nums[i+1]) return false;
+//             continue;
+//         }
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        int count = 0;
-
+        int drop_count = 0;
+        
         for (int i = 0; i < n; i++) {
             if (nums[i] > nums[(i + 1) % n]) {
-                count++;
+                drop_count++;
             }
         }
-
-        return count <= 1;
+        
+        return drop_count <= 1;
     }
 };
