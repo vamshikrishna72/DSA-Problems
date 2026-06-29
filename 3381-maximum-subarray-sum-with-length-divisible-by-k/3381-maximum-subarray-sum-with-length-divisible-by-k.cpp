1346c1d25e1d9a2256d07e0378a3bb47234ea571
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-06-29 14:35:50 +0530

    Time: 3 ms (92.33%), Space: 159.5 MB (64.11%) - LeetHub

---
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maxSubarraySum(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        long long INF = 1e18;
+        std::vector<long long> min_prefix(k, INF);
+        min_prefix[0] = 0;
+        long long current_prefix = 0;
+        long long max_sum = -INF; 
+        
+        for (int i = 0; i < n; ++i) {
+            current_prefix += nums[i];
+            int remainder = (i + 1) % k;
+            
+            if (min_prefix[remainder] != INF) {
+                max_sum = max(max_sum, current_prefix - min_prefix[remainder]);
+            }
+            
+            min_prefix[remainder] = min(min_prefix[remainder], current_prefix);
+        }
+        
+        return max_sum;
+    }
+};
