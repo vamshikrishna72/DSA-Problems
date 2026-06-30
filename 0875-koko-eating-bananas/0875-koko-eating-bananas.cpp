fc00886490854c29d8165300d1081effabd9d862
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-06-30 10:59:53 +0530

    Time: 13 ms (22.83%), Space: 23 MB (47.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,9 +1,40 @@
+// class Solution {
+// public:
+//     int calculateTotalHours(vector<int>& piles, int speed){
+//         int n = piles.size();
+//         int totalH = 0;
+//         for(int bananas : piles){
+//             totalH += ceil((double)bananas/speed);
+//         }
+//         return totalH;
+//     }
+//     int minEatingSpeed(vector<int>& piles, int h) {
+//         // int low = *min_element(piles.begin(),piles.end());
+//         int low = 1;
+//         int high = *max_element(piles.begin(),piles.end());
+//         int ans = high;
+        
+//         while(low <= high){
+//             int mid = (low + (high-low)) / 2;
+//             int totalH = calculateTotalHours(piles,mid);
+//             if(totalH <= h){
+//                 ans = mid;
+//                 high = mid-1;
+//             }
+//             else low = mid+1;
+
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     long long calculateTotalHours(vector<int>& piles, int speed){
         long long totalH = 0;
         for(int bananas : piles){
-            totalH += ((long long)bananas + speed - 1) / speed;
+            totalH += ceil((long long)bananas+speed-1) /speed;
         }
         return totalH;
     }
@@ -14,7 +45,7 @@ public:
         int ans = high;
 
         while(low <= high){
-            int mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2; 
             long long totalH = calculateTotalHours(piles, mid);
 
             if(totalH <= h){
