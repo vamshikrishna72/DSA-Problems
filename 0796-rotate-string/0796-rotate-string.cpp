dbe2433b65404bf5e06f73fcab26e9b35c331c61
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-07-10 15:27:39 +0530

    Time: 3 ms (6.08%), Space: 8.2 MB (51.43%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n=s.length();
-        
-        if(s.length()!=goal.length()) return false;
-        return (s + s).find(goal) != string::npos;
+        if(s.size()!=goal.size()) return false;
+        string temp = s + s;
+        return temp.find(goal) != string::npos;
     }
 };
