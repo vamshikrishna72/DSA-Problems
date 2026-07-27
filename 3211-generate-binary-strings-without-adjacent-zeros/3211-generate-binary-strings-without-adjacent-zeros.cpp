00e68c33afbb9b973570e425b5d2d2b84bf4c5d5
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-07-27 15:02:11 +0530

    Time: 4 ms (94.72%), Space: 22.4 MB (72.53%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    
+public:
+    vector<string> ans;
+    void solve(string &current,int n){
+        if(current.size() == n){
+            ans.push_back(current);
+            return;
+        }
+        if(current.empty() || current.back() == '1'){
+            current.push_back('0');
+            solve(current,n);
+            current.pop_back();
+        }
+        current.push_back('1');
+        solve(current,n);
+        current.pop_back();
+
+        
+    }
+    vector<string> validStrings(int n) {
+        string current = "";
+        solve(current,n);
+        return ans;
+    }
+};
