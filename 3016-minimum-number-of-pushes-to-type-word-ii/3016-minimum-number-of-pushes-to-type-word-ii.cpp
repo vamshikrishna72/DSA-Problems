712eb904543fc2d2cc6e7192d4c038b04d586551
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-07-31 06:41:23 +0530

    Time: 11 ms (48.07%), Space: 25.3 MB (84.45%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+
+        for (char c : word)
+        freq[c - 'a']++;
+
+        sort(freq.begin(), freq.end(), greater<int>());
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if(freq[i] == 0) break;
+
+            int cost = i / 8 + 1;
+            ans += freq[i] * cost;
+        }
+        return ans;
+        
+    }
+};
