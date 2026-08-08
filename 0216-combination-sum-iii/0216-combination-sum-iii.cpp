b1a6788fa3ca219280bc01d6d64b8feaab6a7071
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-08-09 03:28:08 +0530

    Time: 4 ms (6.96%), Space: 14.6 MB (5.37%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void solve(int i,int n,int k,vector<vector<int>>& ans,vector<int> ds){
+        if(k == 0 && n == 0) {
+            ans.push_back(ds);
+            return;
+        }
+        if(i > 9 || n < 0 || k < 0) return;
+    
+        
+        ds.push_back(i);
+        solve(i+1,n-i,k-1,ans,ds);
+        ds.pop_back();
+        solve(i+1,n,k,ans,ds);
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        solve(1,n,k,ans,ds);
+        return ans;
+    }
+};
