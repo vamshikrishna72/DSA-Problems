7183ea8f70b47db3135a21fa5c4a9dd618df6be2
Vamshi Krishna | Google Student Ambassador <153121080+vamshikrishna72@users.noreply.github.com>
2026-09-17 22:44:13 +0530

    Time: 8 ms (70.62%), Space: 88.8 MB (93.56%) - LeetHub

---
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& A, int k) {
+        int n = A.size();
+        int res = n + 1;
+        int sum = 0;
+        int i = 0;
+
+        vector<int> dp(n + 1, n);
+
+        for (int j = 0; j < n; j++) {
+            sum += A[j];
+
+            while (sum > k)
+                sum -= A[i++];
+
+            dp[j + 1] = dp[j];
+
+            if (sum == k) {
+                res = min(res, j - i + 1 + dp[i]);
+                dp[j + 1] = min(dp[j], j - i + 1);
+            }
+        }
+
+        return res == n + 1 ? -1 : res;
+    }
+};
